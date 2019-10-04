64daef5d288eb291cb18b4016bc365006bb4f1ac
hsisco <hysensisco@gmail.com>
2019-10-04 08:11:59 -0700

    Tabs Js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -5,5 +5,30 @@
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
-//  The tab component should look like this:
+// The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const tabs = document.querySelector('.tabs');
+const topics = document.querySelector('.topics');
+const title = document.querySelector('.title');
+let topicsArr = [];
+
+function createTab(i){
+  const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = i;
+  console.log(i)
+  return tab;
+};
+
+axios
+.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then (response => {
+  topicsArr = response.data.topics;
+  console.log(topicsArr);
+  console.log(tabs);
+  topicsArr.forEach(i => {
+    topics.appendChild(createTab(i));
+  })})
+.catch (error => {
+  console.log('Nope...thats not right.', error)})
